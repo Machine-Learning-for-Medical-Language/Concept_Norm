4e30041930944fc80cef9bbf537002449661ef03
dongfang91 <xudffaint@gmail.com>
2021-04-18 03:35:39 -0400

    update bash file

--- run_model.sh
+++ run_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=chip-gpu             # queue to be used
 #SBATCH --account=chip
-#SBATCH --time=1:00:00             # Running time (in hours-minutes-seconds)
+#SBATCH --time=12:00:00             # Running time (in hours-minutes-seconds)
 #SBATCH --job-name=conorm             # Job name
 #SBATCH --mail-type=BEGIN,END,FAIL      # send and email when the job begins, ends or fails
 #SBATCH --mail-user=dongfang.xu@childrens.harvard.edu      # Email address to send the job status
@@ -15,10 +15,10 @@ pwd; hostname; date
 
 module load singularity
 
-margins='0.3'
+margins='0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7'
 for margin in $margins
 do
-    MARGIN=$margin
+
     OUTPUT_DIR=/temp_work/ch223150/outputs/joint_model/e15_b16_seq128_5e5_sc30_m$margin
     
     singularity exec -B $TEMP_WORK --nv /temp_work/ch223150/image/hpc-ml_centos7-python3.7-transformers4.4.1.sif  python3.7 train_system_joint.py \
@@ -30,15 +30,15 @@ do
         --do_eval \
         --do_predict \
         --train_batch_size 16 \
-        --num_train_epochs 2 \
+        --num_train_epochs 15 \
         --overwrite_output_dir true \
         --overwrite_cache true \
         --max_seq_length 128 \
         --token true \
         --label_names st_labels \
         --pad_to_max_length true \
-        --learning_rate 5e-5
-        --margin $MARGIN \
+        --learning_rate 5e-5 \
+        --margin $margin \
         --scale 30 \
         --evals_per_epoch 1
 
