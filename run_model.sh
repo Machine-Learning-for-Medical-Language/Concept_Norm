fc95ef9eea2e0110979927252ebd6b7f96a96faf
dongfang91 <xudffaint@gmail.com>
2021-04-27 12:44:34 -0400

    load weights from the encoder

--- run_model.sh
+++ run_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=chip-gpu             # queue to be used
 #SBATCH --account=chip
-#SBATCH --time=36:00:00             # Running time (in hours-minutes-seconds)
+#SBATCH --time=8:00:00             # Running time (in hours-minutes-seconds)
 #SBATCH --job-name=conorm             # Job name
 #SBATCH --mail-type=BEGIN,END,FAIL      # send and email when the job begins, ends or fails
 #SBATCH --mail-user=dongfang.xu@childrens.harvard.edu      # Email address to send the job status
@@ -16,25 +16,25 @@ pwd; hostname; date
 module load singularity
 
 
-OUTPUT_DIR=/temp_work/ch223150/outputs/joint_model/ontology_all_e24_b256_seq16_1e4_sc45_m0.35
+OUTPUT_DIR=/temp_work/ch223150/outputs/joint_model/continue_1_ontology+train_all_e24_b400_seq16_1e4_sc45_m0.35
     
 singularity exec -B $TEMP_WORK --nv /temp_work/ch223150/image/hpc-ml_centos7-python3.7-transformers4.4.1.sif  python3.7 train_system_joint.py \
-        --model_name_or_path /home/ch223150/projects/models/0_Transformer \
-        --data_dir /home/ch223150/projects/Concept_Norm/data/n2c2/joint_input/umls/ \
+        --model_name_or_path /home/ch223150/projects/models/umls_bert_0/ \
+        --data_dir /home/ch223150/projects/Concept_Norm/data/n2c2/joint_input/umls+data/ \
         --output_dir $OUTPUT_DIR \
         --task_name st_joint \
         --do_train \
         --do_eval \
         --do_predict \
-        --train_batch_size 256 \
-        --num_train_epochs 24 \
+        --train_batch_size 400 \
+        --num_train_epochs 3 \
         --overwrite_output_dir true \
         --overwrite_cache false \
         --max_seq_length 16 \
         --token true \
         --label_names st_labels \
         --pad_to_max_length true \
-        --learning_rate 1e-4 \
+        --learning_rate 5e-5 \
         --margin 0.35 \
         --scale 45 \
         --evals_per_epoch 1
